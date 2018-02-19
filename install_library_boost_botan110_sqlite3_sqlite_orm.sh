db50b224e76c6b143be44eff61c64c4e967b7d25
sloth-dev <36595130+sloth-dev@users.noreply.github.com>
2018-02-20 06:48:43 +0800

    Update install_library_boost_botan110_sqlite3_sqlite_orm.sh

--- install_library_boost_botan110_sqlite3_sqlite_orm.sh
+++ install_library_boost_botan110_sqlite3_sqlite_orm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 apt-get install libboost-all-dev libbotan1.10-dev libsqlite3-dev -y
 wget https://raw.githubusercontent.com/fnc12/sqlite_orm/master/include/sqlite_orm/sqlite_orm.h
 cp sqlite_orm.h /usr/include
