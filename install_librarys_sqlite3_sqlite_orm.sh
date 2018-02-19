fc59ca1802744fcc36fd804213fb845ec19bc413
sloth-dev <36595130+sloth-dev@users.noreply.github.com>
2018-02-20 04:53:54 +0800

    Update install_librarys_sqlite3_sqlite_orm.sh

--- install_librarys_sqlite3_sqlite_orm.sh
+++ install_librarys_sqlite3_sqlite_orm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install libsqlite3-dev
+apt-get install libboost-all-dev libbotan1.10-dev libsqlite3-dev -y
 wget https://raw.githubusercontent.com/fnc12/sqlite_orm/master/include/sqlite_orm/sqlite_orm.h
 cp sqlite_orm.h /usr/include
 rm sqlite_orm.h
