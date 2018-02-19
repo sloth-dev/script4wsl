3e101052cfc47f6e520a6ad02c5286948525b18b
sloth-dev <36595130+sloth-dev@users.noreply.github.com>
2018-02-20 03:18:42 +0800

    Update and rename install_sqlite3_sqlite_orm to install_librarys_sqlite3_sqlite_orm.sh

--- install_librarys_sqlite3_sqlite_orm.sh
+++ install_librarys_sqlite3_sqlite_orm.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 apt-get install libsqlite3-dev
-
 wget https://raw.githubusercontent.com/fnc12/sqlite_orm/master/include/sqlite_orm/sqlite_orm.h
-
 cp sqlite_orm.h /usr/include
+rm sqlite_orm.h
